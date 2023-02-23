7fbc20a6d03a77d91a3a61f27197cba611d9fd75
Roksolana96 <roxolana.busko@gmail.com>
2023-02-23 19:09:47 +0100

    HW1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const setGellery = document.querySelector('.gallery');
+const cardsMakeup = createImageItem(galleryItems);
+
+setGellery.insertAdjacentHTML('beforeend', cardsMakeup);
+setGellery.addEventListener('click', onImageClick)
+
+
+
+function createImageItem(galleryItems) {
+
+    return galleryItems.map(({preview, original,description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    title="${description}"
+                />
+            </a>
+        </div>
+        `;
+    }).join('');
+};
+
+function onImageClick(event){
+    event.preventDefault(); 
+    if(!event.target.classList.contains('gallery__image')){
+        return;
+    }
+
+    var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250});
+
+};
+
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
